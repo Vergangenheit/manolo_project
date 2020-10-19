3e523fa330aeeb355f00bd65b6e1361984e5328e
Vergangenheit <lorenzo.ostano@gmail.com>
2020-10-19 20:24:38 +0200

    corrected setup again

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]\n
-email = \"m_macchetta@yahoo.com\"\n
+echo "[general]
+email = \"m_macchetta@yahoo.com\"
 " > ~/.streamlit/credentials.toml
-echo "
-[server]\n
-headless = true\n
-enableCORS=false\n
-port = $PORT\n
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
